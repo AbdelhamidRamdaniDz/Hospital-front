8bca8a9e72204f6d664fa0ddf8959e0b618273d8
abdelhamid <165813079+AbdelhamidRamdaniDz@users.noreply.github.com>
2025-07-28 19:06:18 +0100

    Update axios.ts
    update axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,20 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   withCredentials: true,
+// });
+
+// export default API;
+
 import axios from 'axios';
 
+// استخدم متغير البيئة NEXT_PUBLIC_API_URL
+// هذا المتغير سيتم تعيينه في next.config.js (للبناء المحلي) وعلى Vercel (للنشر)
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_BASE_URL, // تم التغيير هنا
   withCredentials: true,
 });
 
